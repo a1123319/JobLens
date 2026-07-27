6a39a65b6a7842b2948a1e73cab568fc94618bfa
阿斯匹靈 <steven940103@gmail.com>
2026-07-28 00:19:28 +0800

    revert: restore supply-chain/script.js to original codebase

--- supply-chain/script.js
+++ supply-chain/script.js
@@ -25,20 +25,12 @@ function fromCompanyDatabase(entities) {
 	const sectors = new Map();
 
 	for (const entity of entities) {
-		if (entity.Sector) {
-			if (!sectors.has(entity.Sector)) {
-				sectors.set(entity.Sector, []);
-			}
-			sectors.get(entity.Sector).push(entity);
+		if (!sectors.has(entity.Sector)) {
+			sectors.set(entity.Sector, []);
 		}
-
-		const sub = entity.Subsector || entity.SubSector;
-		if (sub && sub !== entity.Sector) {
-			if (!sectors.has(sub)) {
-				sectors.set(sub, []);
-			}
-			sectors.get(sub).push(entity);
-		}
+		
+		const sector = sectors.get(entity.Sector);
+		sector.push(entity);
 	}
 
 	return sectors;
